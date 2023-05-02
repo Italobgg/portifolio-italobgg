43b1bfe2d5430fcda2110d726115d7ab1e29d64a
Italo Bartolomeu <italobggdev@gmail.com>
2023-05-01 21:51:01 -0300

    add cv para e novo style de icons

---
+++ src/js/download-button.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var downloadButton = document.getElementById("download-button");
+  downloadButton.addEventListener("click", function() {
+    window.open("./src/cv/Italo Bartolomeu.pdf", "_blank");
+  });
