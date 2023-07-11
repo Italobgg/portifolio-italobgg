0fcda0aa2f666fae50127dbc490841ddaf1b40e9
Italo Bartolomeu <italobggdev@gmail.com>
2023-07-11 17:05:52 -0300

    reboot

--- src/js/download-button.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var downloadButton = document.getElementById("download-button");
-  downloadButton.addEventListener("click", function() {
-    window.open("./src/cv/Italo Bartolomeu.pdf", "_blank");
-  });
