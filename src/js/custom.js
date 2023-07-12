b3b0beedea54f1b56522ef2d8ccba14673804da9
Italo Bartolomeu <italobggdev@gmail.com>
2023-07-12 16:48:45 -0300

    add fotos e animação

---
+++ src/js/custom.js
@@ -0,0 +1,9 @@
+  var typed = new Typed('.typed-text', {
+    strings: ['Desenvolvedor <i>Web</i>', 'Desenvolvedor <i>Front-End</i>', 'Desenvolvedor <i>Wordpress</i>'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+  
